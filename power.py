b6d2331b77dd0a835a96b1f0929874b0cb887d27
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-09-19 21:19:42 -0500

    Update power.py
    Now can handle bad input

--- power.py
+++ power.py
@@ -7,15 +7,31 @@ def a_power_b(a, b):
     return result
 
 
+def _input(msg, is_b=False):
+    while True:
+        try:
+            value = int(input(msg))
+            if is_b:
+                if value < 10000:
+                    return value
+                else:
+                    print("El exponente genera demasiados cilclos para el computador\nExponente < 10000")
+            else:
+                return value
+        except ValueError:
+            pass
+
+
 def main():
     while True:
-        a = int(input("Numero a: "))
+        a = _input("Numero a: ")
         if not a:
             break
-        b = int(input("Numero b: "))
+        b = _input("Numero b: ", True)
+
         result = a_power_b(a, b)
 
-        print(result)
+        print(f"Result: {result}")
 
 
 if __name__ == "__main__":
