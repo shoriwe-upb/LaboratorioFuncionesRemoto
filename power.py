6f2ee3da4dcd86ce07d4b32e38b25b3de49e1abd
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-09-19 21:28:18 -0500

    Update power.py
    Now can handle negative powers and can show the number of errors and the number of even and odd results

--- power.py
+++ power.py
@@ -1,37 +1,52 @@
 def a_power_b(a, b):
     result = 1
     mult = 1
-    while mult <= b:
+    while mult <= abs(b):
         result *= a
         mult += 1
+    if b < 0:
+        result = 1 / result
     return result
 
 
 def _input(msg, is_b=False):
+    err = 0
     while True:
         try:
             value = int(input(msg))
             if is_b:
                 if value < 10000:
-                    return value
+                    return err, value
                 else:
                     print("El exponente genera demasiados cilclos para el computador\nExponente < 10000")
             else:
-                return value
+                return err, value
         except ValueError:
-            pass
+            err += 1
 
 
 def main():
+    errs = 0
+    even = 0
+    odds = 0
     while True:
-        a = _input("Numero a: ")
+        err, a = _input("Numero a: ")
+        errs += err
         if not a:
             break
-        b = _input("Numero b: ", True)
+        err, b = _input("Numero b: ", True)
+        errs += err
 
         result = a_power_b(a, b)
-
         print(f"Result: {result}")
+
+        if result % 2:
+            odds += 1
+        else:
+            even += 1
+    print(f"Resultados pares: {even}")
+    print(f"Resultados impares: {odds}")
+    print(f"Numero de errores: {errs}")
 
 
 if __name__ == "__main__":
